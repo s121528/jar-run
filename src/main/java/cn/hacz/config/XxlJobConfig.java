284c180791a5b30aa41c9e37639af70577167752
guodd <18838177689@163.com>
2020-04-16 18:31:11 +0800

    基础开发

--- src/main/java/cn/hacz/config/XxlJobConfig.java
+++ src/main/java/cn/hacz/config/XxlJobConfig.java
@@ -20,7 +20,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @ComponentScan(basePackages = "cn.hacz.jobhandler")
 @Slf4j
-@Profile(value = {"prod"})
 public class XxlJobConfig {
     @Value("${xxl.job.admin.addresses}")
     private String adminAddresses;
